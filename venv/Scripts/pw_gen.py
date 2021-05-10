404940a5202e462f82ef9a43aadc4aa1c8d691be
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2021-05-10 16:41:37 -0600

    primer draft de la generación de contraseñas

---
+++ venv/Scripts/pw_gen.py
@@ -0,0 +1,10 @@
+import hashlib as hash
+
+def pw_gen(plain):
+    r = hash.md5(plain.encode())
+    num = int(r.hexdigest(), 16)
+    width = len(str(num))
+    return((plain + '_' + str(num))[0:50])
+    
+    
+print(pw_gen('r'))
